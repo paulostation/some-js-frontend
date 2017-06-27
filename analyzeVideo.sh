568f85e897379a6b3afef22706d67245a6ee8abb
Paulo Henrique <pauloh@br.ibm.com>
2017-06-27 13:43:33 +0000

    Updated analyzeVideo script

--- analyzeVideo.sh
+++ analyzeVideo.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-ffmpeg -i /root/some-js-frontend/uploadedVideos/first* -r 1/1 'first' + $filename%03d.bmp
-#python /root/openface/demos/compare.py /root/some-js-frontend/uploadedVideos/{first*,second*}
+ffmpeg -i /root/some-js-frontend/uploadedVideos/first* -r 1/1 "/root/some-js-frontend/uploadedImages/first$filename%03d.bmp"
+ffmpeg -i /root/some-js-frontend/uploadedVideos/second* -r 1/1 "/root/some-js-frontend/uploadedImages/second$filename%03d.bmp"
+python /root/openface/demos/compare.py /root/some-js-frontend/uploadedImages/{first*,second*}
