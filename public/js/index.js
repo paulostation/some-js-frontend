99aa99badc37c9d4ed8d2f4afb0b63f3675c845a
Paulo Henrique <pauloh@br.ibm.com>
2017-06-28 18:43:24 +0000

    Photo and Video almost working

--- public/js/index.js
+++ public/js/index.js
@@ -169,3 +169,80 @@ function analyzeStuff2() {
 
     $.ajax(ajaxSettings);
 }
+
+function analyzeStuff3() {
+
+    let success = result => {
+
+        let scoreArray = result.output.split("\n");
+
+        $(".loader").hide();
+
+        let html = "";
+
+        scoreArray.forEach((score, index) => {
+            if (index > 10) return;
+            //skip invalid results
+            if ("" === score) return;
+
+            (score < 1) ?
+            //if score is less than 1, green
+            html = `<div class="green">${score}<br>Same person</div>`
+                //otherwise, red
+                : html = `<div class="red">${score}<br>Different person</div>`;
+
+            $("#result-score2 .result-score:nth-child(" + index + ")").html(html);
+        });
+
+        let promiseArray = [];
+
+        result.fileList.filter(firstRowItems).forEach((file, index) => {
+
+            if (index > 10) return;
+
+            let $img = $("<img>", {
+                src: '/getImage?file=' + file,
+                width: '80px',
+                height: '120px'
+            });
+
+            $("#result-images-first2 .result-image:nth-child(" + index + ")").append($img);
+        });
+
+        result.fileList.filter(secondRowItems).forEach((file, index) => {
+            if (index > 10) return;
+            let $img = $("<img>", {
+                src: '/getImage?file=' + file,
+                width: '80px',
+                height: '120px'
+            });
+            console.log("updating " + file + "on index " + index);
+            $("#result-images-second2 .result-image:nth-child(" + index + ")").append($img);           
+        });
+
+
+        Promise.all(promiseArray)
+            .then(result => {
+                renderImages(result);
+            })
+            .catch(error => {
+                reject(error);
+            });
+
+    };
+
+    let error = error => {
+        console.error("Error while analyzing images: ", error);
+    };
+
+    let ajaxSettings = {
+        type: "GET",
+        url: '/testVideoAndPhoto',
+        success: success,
+        error: error
+    };
+
+    $(".loader").show();
+
+    $.ajax(ajaxSettings);
+}
